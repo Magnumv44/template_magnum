289eccdeaeb250a2a62c11846021bc7a25544f6b
Vitaliy Gnoyoviy <vitaliy1984@gmail.com>
2015-11-08 20:25:15 +0200

    Добалены проверочный метатеги для поисковиков и рейтинга безовасности сайта.

--- magnum/index.php
+++ magnum/index.php
@@ -22,6 +22,9 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" dir="<?php echo $this->direction; ?>">
 	<head>
     	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name='yandex-verification' content='5141454251eba7ae' />
+        <meta name="google-site-verification" content="3EutjLuhAK4xbS7NQzrUoQ6oYs5eTv3nWno9ZiEEdOU" />
+        <meta name="wot-verification" content="0da95e472682f0116875"/>
     	<jdoc:include type="head" />
         <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/system/css/system.css" type="text/css" />
         <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/css/template.css" type="text/css" />
