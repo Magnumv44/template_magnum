7c1d3b252e609765216cd14a6bf4925583571f4c
Magnumv44 <vitaliy1984@gmail.com>
2017-10-28 19:53:02 +0300

    Изменена версия шаблона с 1.5.0 до 1.5.1

--- magnum/index.php
+++ magnum/index.php
@@ -28,15 +28,15 @@
     	<jdoc:include type="head" />
         <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/system/css/system.css" type="text/css" />
         <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/css/template.css" type="text/css" />
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-1697908-1"></script>
         <script>
-			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-			})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
 
-			ga('create', 'UA-1697908-1', 'auto');
-			ga('send', 'pageview');
-		</script>
+            gtag('config', 'UA-1697908-1');
+        </script>
     </head>
 	<body>
     	<!--начало шапки-->
