4852dd7599cdd9af85c1008b1f623e27037bc6c5
Magnumv44 <vitaliy1984@gmail.com>
2025-04-21 14:33:12 +0300

    Виправлення відображення логотипу в мобільній версії для роботи з Joomla Web Asset з додавання мовних констант

--- magnum/html/mod_menu/collapse-default.php
+++ magnum/html/mod_menu/collapse-default.php
@@ -19,8 +19,8 @@ use Joomla\CMS\Language\Text;
 <nav class="navbar navbar-expand-lg" aria-label="<?php echo htmlspecialchars($module->title, ENT_QUOTES, 'UTF-8'); ?>">
 <div class="container-fluid">
     <a class="navbar-brand d-lg-none" href="/">
-                <img src="templates/magnum/images/small-logo.png" alt="Magnum news - Блог IT-шника" />
-                <span class="smal-logo-text">Magnum news</span>
+                <img src="media/templates/site/magnum/images/small-logo.png" alt="<?php echo Text::_('TPL_MAGNUM_SITE_NAME_FULL'); ?>" />
+                <span class="smal-logo-text"><?php echo Text::_('TPL_MAGNUM_SITE_NAME_SHORT'); ?></span>
     </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar<?php echo $module->id; ?>" aria-controls="navbar<?php echo $module->id; ?>" aria-expanded="false" aria-label="<?php echo Text::_('MOD_MENU_TOGGLE'); ?>">
         <span class="bi bi-justify" aria-hidden="true"></span>
