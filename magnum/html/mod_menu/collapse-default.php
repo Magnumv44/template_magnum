5bb727fb1edc8345629bbc387181222433e86153
Magnumv44 <vitaliy1984@gmail.com>
2026-06-04 16:43:00 +0300

    Оновлення перевизначень

--- magnum/html/mod_menu/collapse-default.php
+++ magnum/html/mod_menu/collapse-default.php
@@ -10,6 +10,7 @@
 
 defined('_JEXEC') or die;
 
+use Joomla\CMS\Helper\ModuleHelper;
 use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Language\Text;
 
@@ -17,16 +18,16 @@ use Joomla\CMS\Language\Text;
 ?>
 
 <nav class="navbar navbar-expand-lg" aria-label="<?php echo htmlspecialchars($module->title, ENT_QUOTES, 'UTF-8'); ?>">
-<div class="container-fluid">
-    <a class="navbar-brand d-lg-none" href="/">
-                <img src="media/templates/site/magnum/images/small-logo.png" alt="<?php echo Text::_('TPL_MAGNUM_SITE_NAME_FULL'); ?>" />
-                <span class="smal-logo-text"><?php echo Text::_('TPL_MAGNUM_SITE_NAME_SHORT'); ?></span>
-    </a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar<?php echo $module->id; ?>" aria-controls="navbar<?php echo $module->id; ?>" aria-expanded="false" aria-label="<?php echo Text::_('MOD_MENU_TOGGLE'); ?>">
-        <span class="bi bi-justify" aria-hidden="true"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbar<?php echo $module->id; ?>">
-        <?php require __DIR__ . '/default.php'; ?>
+    <div class="container-fluid">
+        <a class="navbar-brand d-lg-none" href="/">
+                    <img src="media/templates/site/magnum/images/small-logo.png" alt="<?php echo Text::_('TPL_MAGNUM_SITE_NAME_FULL'); ?>" />
+                    <span class="smal-logo-text"><?php echo Text::_('TPL_MAGNUM_SITE_NAME_SHORT'); ?></span>
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar<?php echo $module->id; ?>" aria-controls="navbar<?php echo $module->id; ?>" aria-expanded="false" aria-label="<?php echo Text::_('MOD_MENU_TOGGLE'); ?>">
+            <span class="bi bi-justify" aria-hidden="true"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbar<?php echo $module->id; ?>">
+            <?php require ModuleHelper::getLayoutPath(module: 'mod_menu', layout:'default'); ?>
+        </div>
     </div>
-</div>
 </nav>
